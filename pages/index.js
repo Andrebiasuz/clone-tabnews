4038aaa6b416dc63330eb74dda9b4b7778d8d894
André Biasuz <58049577+Andrebiasuz@users.noreply.github.com>
2023-08-24 16:02:15 +0000

    Test build by changing the sentence

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 function Home() {
-    return <h1>Linda I love you. If you love me back, give me a smile</h1>
+    return <h1>Linda I love you. If you love me back, give me a smile :D </h1>
 }
-
 
 export default Home;
