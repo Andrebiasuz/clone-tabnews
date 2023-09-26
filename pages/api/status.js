c238484f69a7c5583e3160b1e2673ea6010e23e4
André Biasuz <58049577+Andrebiasuz@users.noreply.github.com>
2023-09-26 13:13:43 +0000

    formatting

--- pages/api/status.js
+++ pages/api/status.js
@@ -1,5 +1,7 @@
+// if status 200 then get json message in API
+
 function status(request, response) {
-  response.status(200).json({ key: "I love you bunny" });
+  response.status(200).json({ key: "I love you bunny" }); // used Json because of charset
 }
 
-export default status;
+export default status; // export so next has access to it.
