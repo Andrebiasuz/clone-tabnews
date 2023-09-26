4626fb3b9ee69bf0797c93258de61952ed305ff2
André Biasuz <58049577+Andrebiasuz@users.noreply.github.com>
2023-09-26 13:28:55 +0000

    Revert "formatting"
    This reverts commit c238484f69a7c5583e3160b1e2673ea6010e23e4.

--- pages/api/status.js
+++ pages/api/status.js
@@ -1,7 +1,5 @@
-// if status 200 then get json message in API
-
 function status(request, response) {
-  response.status(200).json({ key: "I love you bunny" }); // used Json because of charset
+  response.status(200).json({ key: "I love you bunny" });
 }
 
-export default status; // export so next has access to it.
+export default status;
